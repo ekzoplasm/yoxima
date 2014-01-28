7773640e9644f98127499af1ac979130aae6bd8e
Fabrice <Fabrice@Diablotin>
2014-01-28 21:19:36 +0100

    deploy process

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,7 +1,5 @@
 set :application, "yoxima"
-# set :domain,      "#{application}.com"
 set :domain,      "91.121.192.4"
-# set :deploy_to,   "/var/www/#{domain}"
 set :deploy_to,   "/var/www/#{application}"
 set :app_path,    "app"
 set :user,        "fabrice"
@@ -11,17 +9,16 @@ set :ssh_options,   :forward_agent => true
 
 set :repository,  "git@github.com:ekzoplasm/#{application}.git"
 set :scm,         :git
+set :scm_password, "psy5mdna7"
+set :scm_passphrase, "RVapt3FOA"
 set :branch, "master"
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
-# Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
-set :use_composer, true
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
