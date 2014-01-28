82737c8de4145abde48f4ba58ad5fbaba6af230d
Fabrice <Fabrice@Diablotin>
2014-01-28 14:41:02 +0100

    deploy process

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,3 +1,5 @@
+default_run_options[:pty] = true  # Must be set for the password prompt
+                                  # from git to work
 set :application, "yoxima"
 # set :domain,      "#{application}.com"
 set :domain,      "91.121.192.4"
@@ -5,13 +7,15 @@ set :domain,      "91.121.192.4"
 set :deploy_to,   "/var/www/#{application}"
 set :app_path,    "app"
 set :user,        "fabrice"
+set :scm_passphrase, "RVapt3FOA"
 set :use_sudo,    true
 
 set :ssh_options,   :forward_agent => true 
-set :ssh_options,   :keys => %w(c:/Users/fabrice/.ssh/id_rsa)
+set :ssh_options,   :keys => %w(c:/Users/Fabrice/.ssh/id_rsa)
 
 set :repository,  "git@github.com:ekzoplasm/#{application}.git"
 set :scm,         :git
+set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
